a79455ba3a8dfb7e83162dffcb60cc190520b560
20-08741 <20-08741@g.batstate-u.edu.ph>
2026-06-23 08:27:20 +0800

    setup frontend architecture and tailwind integration

--- campusflow-web/src/modules/dashboard/views/DashboardView.vue
+++ campusflow-web/src/modules/dashboard/views/DashboardView.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import AppLayout from '../../../shared/layouts/AppLayout.vue'
+
+  import AppLayout from '../../../shared/layouts/AppLayout.vue'
+  
 </script>
 
 <template>
-
   <AppLayout>
-
-    <h1>
-
-      Dashboard
-
-    </h1>
-
+    <div class="bg-blue-500 p-8 rounded-lg">
+      <h1 class="text-white text-3xl">
+        Dashboard
+      </h1>
+    </div>
   </AppLayout>
-
 </template>
