7e412f0cb060f1a45683bd8d7ef4cfa53d133d2e
20-08741 <20-08741@g.batstate-u.edu.ph>
2026-06-23 15:52:51 +0800

    feat: add theme toggle and clock components, update layout with new features

---
+++ campusflow-web/src/shared/components/ThemeToggle.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Sun,Moon } from '@lucide/vue'
+
+import { useThemeStore } from '../../stores/themeStore'
+
+const themeStore =
+  useThemeStore()
+</script>
+
+<template>
+
+  <button
+    @click="themeStore.toggleTheme()"
+    class="text-gray-500 hover:text-gray-800 transition"
+  >
+
+    <Moon
+      v-if="!themeStore.darkMode"
+      :size="18"
+    />
+
+    <Sun
+      v-else
+      :size="18"
+    />
+
+  </button>
+
+</template>
